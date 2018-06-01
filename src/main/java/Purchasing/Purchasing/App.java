503485bed6b7cbfa95f865933b5ef40a3321d7e5
clydeharold <ch.paronda@gmail.com>
2018-06-01 18:25:48 +0800

    added security

--- src/main/java/Purchasing/Purchasing/App.java
+++ src/main/java/Purchasing/Purchasing/App.java
@@ -2,6 +2,13 @@ package Purchasing.Purchasing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Hello world!
@@ -14,4 +21,50 @@ public class App
     {
         SpringApplication.run(App.class, args);
     }
+    
+    @Configuration
+	@EnableGlobalMethodSecurity(prePostEnabled = true)
+	@EnableWebSecurity
+	static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+		/**
+		 * This section defines the user accounts which can be used for authentication as well as the roles each user has.
+		 * 
+		 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder)
+		 */
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+			auth.inMemoryAuthentication().//
+					withUser("joli_user").password("password").roles("USER").and().//
+					withUser("joli_admin").password("password").roles("USER", "ADMIN");
+		}
+		
+		
+		
+
+		/**
+		 * This section defines the security policy for the app.
+		 * <p>
+		 * <ul>
+		 * <li>BASIC authentication is supported (enough for this REST-based demo).</li>
+		 * <li>/employees is secured using URL security shown below.</li>
+		 * <li>CSRF headers are disabled since we are only testing the REST interface, not a web one.</li>
+		 * </ul>
+		 * NOTE: GET is not shown which defaults to permitted.
+		 *
+		 * @param http
+		 * @throws Exception
+		 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.web.builders.HttpSecurity)
+		 */
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+
+			http.httpBasic().and().authorizeRequests().//
+					antMatchers(HttpMethod.POST, "/api/joliuser").hasAnyRole("ADMIN","USER").//
+					antMatchers(HttpMethod.PUT, "/api/joliuser/**").hasAnyRole("ADMIN","USER").//
+					antMatchers(HttpMethod.PATCH, "//api/joliuser/**").hasAnyRole("ADMIN","USER").and().//
+					csrf().disable();
+		}
+	}
 }
